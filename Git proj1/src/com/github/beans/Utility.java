50c2277707712fc0e63ee06f1c8583e100fe2009
TL <TL@gmail.com>
2020-04-23 12:49:56 +0530

    kjklfg

--- Git proj1/src/com/github/beans/Utility.java
+++ Git proj1/src/com/github/beans/Utility.java
@@ -14,8 +14,21 @@ public class Utility {
 		System.out.println("Utility.m1()....abc");
 		System.out.println("abc");
 	}
+
 	public void m5()
 	{
 		System.out.println("Utility.m5()");
 	}
+
+	public void m2()
+	{
+		System.out.println("Utility.m2()");
+	}
+	public void m3() {
+		System.out.println("m3");
+	}
+	public void m4() {
+		System.out.println("m4");
+	}
+
 }
